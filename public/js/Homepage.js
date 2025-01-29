773fd6e7cb413e7d793cebed166f27f5becbf6df
Andrew Sirico <siricoa1@montclair.edu>
2025-01-28 21:20:58 -0500

    CSS changes
    Sprucing up the homepage with some custom made css and some text to fill the homepage with.

--- public/js/Homepage.js
+++ public/js/Homepage.js
@@ -3,9 +3,24 @@ const Homepage = function () {
     homePageContainer.id = 'homePageContainer';
 
     const homeTitle = document.createElement('h1');
-    homeTitle.textContent = 'Welcome Home';
+    homeTitle.textContent = 'Welcome';
+
+    const homeBodyDiv = document.createElement('div');
+    homeBodyDiv.id = 'homeBodyDiv';
+
+    const greetingsText = document.createElement('p');
+    greetingsText.id = 'homeGreetingsText';
+    greetingsText.innerText = 
+    'Hi, my name is Andrew Sirico and I am a software engineer who specializes in web development.'+
+    ' I am also experienced writing programs and scripts in Python, Java, and C. Furthermore, my experience extends to '+
+    'SwiftUI, Android Development, and using AWS EC2 instances. I am comfortable querying in SQL, Mongodb, firebase, and GraphQL.'+
+    ' Also, I have utilized firebase, am comfortable with using Github, have experience with Jira, and comfortable participating in SCRUM meetings.' +
+    ' I am highly sociable, carry a professional demeanor, and have high empathy. I strive to be self-aware and to keep my ego in check.'+
+    ' The people I look up to most throughout history are Marcus Aurelius, Diogenes of Sinope, Friedrich Nietzsche, George Washington, Tatanka Iyotake, and Siddhartha Gautama'
 
     homePageContainer.appendChild(homeTitle);
+    homePageContainer.appendChild(homeBodyDiv);
+    homeBodyDiv.appendChild(greetingsText);
 
     const render = function (parentElement) {
         parentElement.innerHTML = '';
