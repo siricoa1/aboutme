ce4655bfe7695f48fd07642204c1e6f8ec4f8413
Andrew Sirico <siricoa1@montclair.edu>
2025-01-29 13:44:17 -0500

    Image added
    Added an image to the homepage and made some small CSS changes to the font and image.

--- public/js/Homepage.js
+++ public/js/Homepage.js
@@ -8,18 +8,21 @@ const Homepage = function () {
     const homeBodyDiv = document.createElement('div');
     homeBodyDiv.id = 'homeBodyDiv';
 
+    const imageContainer = document.createElement('img');
+    imageContainer.id = 'homeImageContainer';
+    imageContainer.src = '../images/IMG_4179.JPG'
+
+    const imageDiv = document.createElement('div');
+    imageDiv.id = 'homeImgDiv';
+
     const greetingsText = document.createElement('p');
     greetingsText.id = 'homeGreetingsText';
-    greetingsText.innerText = 
-    'Hi, my name is Andrew Sirico and I am a software engineer who specializes in web development.'+
-    ' I am also experienced writing programs and scripts in Python, Java, and C. Furthermore, my experience extends to '+
-    'SwiftUI, Android Development, and using AWS EC2 instances. I am comfortable querying in SQL, Mongodb, firebase, and GraphQL.'+
-    ' Also, I have utilized firebase, am comfortable with using Github, have experience with Jira, and comfortable participating in SCRUM meetings.' +
-    ' I am highly sociable, carry a professional demeanor, and have high empathy. I strive to be self-aware and to keep my ego in check.'+
-    ' The people I look up to most throughout history are Marcus Aurelius, Diogenes of Sinope, Friedrich Nietzsche, George Washington, Tatanka Iyotake, and Siddhartha Gautama'
+    greetingsText.innerText = 'Hi, my name is Andrew Sirico and I am a software engineer who specializes in web development. I am also experienced writing programs and scripts in Python, Java, and C. Furthermore, my experience extends to SwiftUI, Android Development, and using AWS EC2 instances. I am comfortable querying in SQL, Mongodb, firebase, and GraphQL. Also, I have utilized firebase, am comfortable with using Github, have experience with Jira, and comfortable participating in SCRUM meetings. I am highly sociable, carry a professional demeanor, and have high empathy. I strive to be self-aware and to keep my ego in check. The people I look up to most throughout history are Marcus Aurelius, Diogenes of Sinope, Friedrich Nietzsche, George Washington, Tatanka Iyotake, and Siddhartha Gautama';
 
     homePageContainer.appendChild(homeTitle);
     homePageContainer.appendChild(homeBodyDiv);
+    imageDiv.appendChild(imageContainer);
+    homeBodyDiv.appendChild(imageDiv);
     homeBodyDiv.appendChild(greetingsText);
 
     const render = function (parentElement) {
