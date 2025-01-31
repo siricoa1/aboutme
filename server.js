cb65f093b6b4b70642f9ab844ccbb930aad25b0a
Andrew Sirico <siricoa1@montclair.edu>
2025-01-31 07:56:47 -0500

    Changed server.js for heroku build
    Changed server.js file for Heroku build to accept the port.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 8080
 
 app.use(express.static('public'));
 
@@ -8,6 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/html/index.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
