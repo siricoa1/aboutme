161938c02f2d51500b6cd951ce445dbe908e0ffa
Andrew Sirico <siricoa1@montclair.edu>
2025-02-01 23:45:49 -0500

    Github and LinkedIn links added
    Added github and linkedIn links. Also added email to contact page. CSS made for these elements as well.

--- public/js/Contactpage.js
+++ public/js/Contactpage.js
@@ -5,7 +5,32 @@ const Contactpage = function () {
     const contactTitle = document.createElement('h1');
     contactTitle.textContent = 'Contact';
 
+    const contactPageDiv = document.createElement('div');
+    contactPageDiv.id = 'contactPageDiv';
+
+    const contactPageEmail = document.createElement('h3');
+    contactPageEmail.id = 'contactPageEmail'
+    contactPageEmail.textContent = '12siricoa94@gmail.com'
+    
+    const contactPageLinkedInLink = document.createElement('a');
+    const linkedInLogo = document.createElement('img');
+    linkedInLogo.src = '../images/LI-In-Bug.png';
+    linkedInLogo.id = 'linkedInLogo';
+    contactPageLinkedInLink.appendChild(linkedInLogo);
+    contactPageLinkedInLink.href = 'https://www.linkedin.com/in/andrew-sirico-24611a342/'
+    
+    const contactPageGitHubLink = document.createElement('a');
+    contactPageGitHubLink.href = 'https://github.com/siricoa1';
+    const gitHubLogo = document.createElement('img');
+    gitHubLogo.src = '../images/github-mark.png'
+    gitHubLogo.id = 'gitHubLogo';
+    contactPageGitHubLink.appendChild(gitHubLogo);
+
     contactPageContainer.appendChild(contactTitle);
+    contactPageContainer.appendChild(contactPageDiv);
+    contactPageDiv.appendChild(contactPageEmail);
+    contactPageDiv.appendChild(contactPageLinkedInLink);
+    contactPageDiv.appendChild(contactPageGitHubLink);
 
     const render = function (parentElement) {
         parentElement.innerHTML = '';
