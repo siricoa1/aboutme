f75b622dcd1734f0e9a8fae8d1f02626e1e45162
Andrew Sirico <159565301+siricoa1@users.noreply.github.com>
2025-02-06 21:31:55 -0500

    Updated ProjectList.js
    Added a project to the projects list.

--- public/js/ProjectList.js
+++ public/js/ProjectList.js
@@ -1,5 +1,10 @@
 const ProjectList = [
     {
+        "projectName": "Node Chat App",
+        "projectDescription":"This application is a build from the Book "+'"Node.js in Action"'+". It allows users to create chat rooms, custom nicknames, and to chat with eachother accross the web.",
+        "projectLink":"https://nodeinaction-chat-app-08f6f785d014.herokuapp.com/"
+    },
+    {
         "projectName": "Currency Exchanger",
         "projectDescription":"This application can compare curriencies values against eachother. Built on Javascript and run in a Node/Express environment, it utilizes a simple API to fetch data about currencies." ,
         "projectLink":"https://currencyexchanger-f4fb49e5e66a.herokuapp.com/"
